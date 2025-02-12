3774cf1463e91cc25a33963cbe5baaccabd7361e
arthur <165952196+WinzLow95@users.noreply.github.com>
2025-02-12 00:18:26 -0300

    Add files via upload

---
+++ jeff.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "---------------------------"
+echo "-    desafio do jeff      -"
+echo "---------------------------"
+echo "[!] um arquivo chamado [jeff.txt] foi criado!"
+echo "[!] o seu objetivo é encontrar esse arquivo e ver o conteúdo dele!"
+echo ""
+echo "[x] ver o código fonte desse arquivo não vale!"
+echo ""
+echo "boa sorte! ~porto"
+
+echo "o arquivo que você tem que pegar agora está com o super usuário do sistema" > /home/..secret/jeff.txt
+echo "IyMjIyMjIyMgICAgICMjIyAgICAjIyMjIyMjIyAgICAgIyMjICAgICMjIyMjIyMjICAjIyMjIyMj IyAjIyAgICAjIyAgIyMjIyMjICAKIyMgICAgICMjICAgIyMgIyMgICAjIyAgICAgIyMgICAjIyAj IyAgICMjICAgICAjIyAjIyAgICAgICAjIyMgICAjIyAjIyAgICAjIyAKIyMgICAgICMjICAjIyAg ICMjICAjIyAgICAgIyMgICMjICAgIyMgICMjICAgICAjIyAjIyAgICAgICAjIyMjICAjIyAjIyAg ICAgICAKIyMjIyMjIyMgICMjICAgICAjIyAjIyMjIyMjIyAgIyMgICAgICMjICMjIyMjIyMjICAj IyMjIyMgICAjIyAjIyAjIyAgIyMjIyMjICAKIyMgICAgICAgICMjIyMjIyMjIyAjIyAgICMjICAg IyMjIyMjIyMjICMjICAgICAjIy AjIyAgICAgICAjIyAgIyMjIyAgICAgICAjIy AKIyMgICAgICAgICMjICAgICAjIy AjIy AgICAjIy AgIyMgICAgICMjICMjICAgICAjIy AjIy AgICAgICAjIy AgICMjIy AjIy AgICAjIyAKIyMgICAgICAgICMjICAgICAjIy AjIy AgICAgIyMgIyMgICAgICMjICMjIyMj IyMjICAjIyMjIyMjIy AjIyAgICAjIy AgIyMjIyMj IC AKCgp2b2NlIGFwcmVuZGV1IG9zIGNvbWFuZG8gYmFzaWNvcyBkbyB0ZXJtaW5hbAoKYXR1YWxpem91IG8gc2V1IHNpc3RlbWEKYmFpeG91IHByb2pldG9zIGRvIGdpdGh1YiBwZWxvIHRlcm1pbmFsCm5hdmVnb3UgcGVsb3MgZGlyZXRvcmlvcywKdmlyb3Ugc3VwZXIgdXN1YXJpbyBkbyBzaXN0ZW1hCmUgYWJyaXUgYXJxdWl2b3MgY29tbyBhZG1pbmlzdHJhZG9yLgplIGRlc2NyaXB0b2dyYWZvdSB1bWEgbWVuc2FnZW0gY29tIG8gYmFzZTY0CgphZ29yYSB2b2NlIGrDoSBwb2RlIHVzYXIgdW0gdGVybWluYWwgc2VtIHRlciBtdWl0byBtZWRvCg==" > /root/root.txt
+sudo chown root:root /root/root.txt
+sudo chmod 600 /root/root.txt
+
+exit
